4b2c0a78b35dc1a230ebf3ee006198f539f4937d
banniford <790032382@qq.com>
2025-02-27 17:49:23 +0800

    init ui,test

--- src/app.py
+++ src/app.py
@@ -1,11 +1,11 @@
 import sys
 
-from control.HandleFunc import HandleFunc
+from control.HandleFunc import MainWindow
 from PyQt6.QtWidgets import QApplication
 
 # 注意修改./ui/picturetoolUI.py 文件中.ico的路径为 ./ 以main文件为起始地址
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    Window = HandleFunc()
+    Window = MainWindow()
     Window.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
--- src/control/HandleFunc.py
+++ src/control/HandleFunc.py
@@ -1,9 +1,10 @@
 
-
-from PyQt6.QtWidgets import QMainWindow, QFileDialog
-
 import warnings
 warnings.filterwarnings("ignore")
+from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog
+from PyQt6.uic import loadUi
 
-class HandleFunc(QMainWindow):
-    pass
+class MainWindow(QMainWindow):
+    def __init__(self, ui_path):
+        super().__init__()
+        loadUi(ui_path, self)
