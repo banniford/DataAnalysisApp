fix:read csv,add precision

src/service/FileManager.py
@@ -37,7 +37,7 @@ class FileManager:
                 # 如果检测不到合适的表头行，你可以选择报错或给个默认值
                 self.main_window.msg("未找到稳定的表头行，请检查文件格式")
                 return
-            print(f"检测到表头行号：{header_line_index}")
+            self.main_window.msg(f"检测到表头行号：{header_line_index} 行。")
             # 2) 用 skiprows 指定要跳过的行，让 pandas 从检测到的那行开始当作表头
             self.df = pd.read_csv(
                 self.file_path[0],
@@ -54,6 +54,8 @@ class FileManager:
             self.df.dropna(axis=0, how='all', inplace=True)
             # ★ 只保留数值型的列（float / int）
             self.df = self.df.select_dtypes(include=[np.number])
+
+            self.main_window.msg(f"成功加载数据列表：{self.df.columns.tolist()}")
 
             # 5) 将 DataFrame 交给你的数据分析模块
             self.main_window.draw.data_analysis.set_table_data(self.df)
@@ -80,7 +82,12 @@ class FileManager:
 
     def addComboBoxItems(self, items: list):
         self.main_ui.comboBox2_1.addCheckableItems(items)
+        self.main_ui.comboBox2_1.setCurrentIndex(-1)  # 设置默认不选中任何选项
+        self.main_ui.comboBox2_1.lineEdit().clear()  # 确保 lineEdit 初始状态为空
         self.main_ui.comboBox2_2.addCheckableItems(items)
+        self.main_ui.comboBox2_2.setCurrentIndex(-1)  # 设置默认不选中任何选项
+        self.main_ui.comboBox2_2.lineEdit().clear()  # 确保 lineEdit 初始状态为空
+        
 
     # -------------------------------------------------------
     # 下面是通用的“自动检测表头行”函数 & 表头符号清洗函数

src/service/ReportTable.py
@@ -2,6 +2,7 @@ from PyQt6.QtWidgets import QAbstractItemView, QHeaderView, QAbstractScrollArea,
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QKeySequence
 from service.DataAnalysis import DataAnalysis
+import numpy as np
 
 class ReportTable:
     def __init__(self, main_window,data_analysis:DataAnalysis):
@@ -12,6 +13,7 @@ class ReportTable:
         self.table_data = {}
         # 小数点精度
         self._precision = 2
+        self.cal_list = []
 
 
         # 设置表格大小调整为内容适应
@@ -40,46 +42,51 @@ class ReportTable:
     @precision.setter
     def precision(self, value):
         self._precision = value
-        # self.update_table_data()
-        pass
+        self.update_table(self.cal_list)
 
     def update_precision(self, precision):
         """更新小数点精度"""
         self.precision = precision
 
-    def update_table(self, name):
+    def update_table(self, cal_list:list):
         """更新表格数据"""
-        if name not in self.table_data:
-            self.table_data[name] = {"avg": [], "max_min": []}
+        self.cal_list = cal_list
+        for i in cal_list:
+            if i not in self.table_data:
+                    self.table_data[i] = {"avg": [], "max_min": []}
         
-        self.table_data[name]["avg"] = self.data_analysis.data_avg[name]
-        self.table_data[name]["max_min"] = self.data_analysis.data_max_min[name]
-        self.show_data(name)
-
-    def show_data(self, name):
-        """显示数据"""
-        # 检查当前name是否等于 comboBox2_3 的值
-        if self.main_ui.comboBox2_3.currentText() != name and self.data_analysis.stable_interval.get(name) is None:
-            return
-        
-        
+            # 平均数根据小数点精度进行四舍五入
+            self.table_data[i]["avg"] = [
+                np.floor(value * (10 ** self.precision)) / (10 ** self.precision) 
+                for value in self.data_analysis.data_avg[i]
+            ]
+            self.table_data[i]["max_min"] = self.data_analysis.data_max_min[i]
+        # 清空表格
         self.clear_all_columns()
+        # 显示数据
+        self.show_data(cal_list)
+
+    def show_data(self,cal_list:list):
+        """显示数据"""
         # 添加第一列为稳定阶段的数据
-        if not self.data_analysis.stable_interval[name]:
+        if not self.data_analysis.stable_interval[cal_list[0]]:
             self.add_column("稳定阶段", [f"0 - {self.data_analysis.get_table_num()-1}"])
             self.data_analysis.get_table_columns()
         else:
-            self.add_column("稳定阶段", [f"{interval[0]} - {interval[1]}" for interval in self.data_analysis.stable_interval[name]])
-        # 添加第三列为平均值
-        self.add_column("平均值", self.table_data[name]["avg"])
-        # 添加第四列为最大值索引
-        self.add_column("最大值索引", [f"{max_min[1]}" for max_min in self.table_data[name]['max_min']])
-        # 添加第五列为最大值
-        self.add_column("最大值", [f"{max_min[0]}" for max_min in self.table_data[name]['max_min']])
-        # 添加第六列为最小值索引
-        self.add_column("最小值索引", [f"{max_min[3]}" for max_min in self.table_data[name]['max_min']])
-        # 添加第七列为最小值
-        self.add_column("最小值", [f"{max_min[2]}" for max_min in self.table_data[name]['max_min']])
+            self.add_column("稳定阶段", [f"{interval[0]} - {interval[1]}" for interval in self.data_analysis.stable_interval[cal_list[0]]])
+        # 添加第二列主次变量得参数 名称+平均值 在前面
+        for i in cal_list:
+            self.add_column(f"{i} 平均值", self.table_data[i]["avg"])
+
+        for i in cal_list:
+             # 添加第四列为主变量最大值索引
+            self.add_column(f"{i} 最大值索引", [f"{max_min[1]}" for max_min in self.table_data[i]['max_min']])
+            # 添加第五列为最大值
+            self.add_column(f"{i} 最大值", [f"{np.floor(max_min[0] * (10 ** self.precision)) / (10 ** self.precision)}" for max_min in self.table_data[i]['max_min']])
+           # 添加第六列为最小值索引
+            self.add_column(f"{i} 最小值索引", [f"{max_min[3]}" for max_min in self.table_data[i]['max_min']])
+            # 添加第七列为最小值
+            self.add_column(f"{i} 最小值", [f"{np.floor(max_min[2] * (10 ** self.precision)) / (10 ** self.precision)}" for max_min in self.table_data[i]['max_min']])
 
 
     def show_context_menu(self, position):
@@ -165,6 +172,6 @@ class ReportTable:
         """清空所有行"""
         self.table.setRowCount(0)
 
-    def clear_all_columns(self,name):
-        """清空name下的所有列"""
+    def clear_all_columns(self):
+        """清空下的所有列"""
         self.table.setColumnCount(0)